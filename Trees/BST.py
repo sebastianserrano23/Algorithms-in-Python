09b1c34f5fcc96c2d7bd1d8d56dc0d4dc6ea481f
Sebastian Serrano <serranosebastian23@gmail.com>
2023-03-21 15:30:15 -0700

    minor

--- Trees/BST.py
+++ Trees/BST.py
@@ -1,4 +1,4 @@
-#i will implement a simple binary search tree in python
+# I will implement a simple binary search tree in python
 # we start off with a root node (lets say 6), in a binary search tree
 # as we go deeper into the tree , every value less than the root/parent node will be on the left side
 # every value greater than or equal to the root/parent node will be on the right side
