716f4e83aaf70f8f108d467cf6030d713b37c1f0
Sebastian Serrano <serranosebastian23@gmail.com>
2023-03-21 15:28:33 -0700

    added a new folder named tree, where I will be implementing trees in python, with a file called BST that implements a simple recursive binary search tree algorithm that checks if the value given is less than self.value, and then adds it to the appropriate location

---
+++ Trees/BST.py
@@ -0,0 +1,29 @@
+#i will implement a simple binary search tree in python
+# we start off with a root node (lets say 6), in a binary search tree
+# as we go deeper into the tree , every value less than the root/parent node will be on the left side
+# every value greater than or equal to the root/parent node will be on the right side
+# each child node can be viewed as its own binary tree, to make the function recursive, same rules apply
+
+
+from asyncio.windows_events import NULL
+
+
+class TreeNode:
+
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = TreeNode(value)
+            else:
+                self.left.inser(value)
+
+        else:
+            if self.right is None:
+                self.right = TreeNode(value)
+            else:
+                self.right.insert(value)
